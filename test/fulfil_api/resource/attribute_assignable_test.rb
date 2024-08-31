6d25d2de75ff6ab3676348949fe7841f8b434e4b
Stefan Vermaas <stefan@codeture.nl>
2024-08-30 19:55:38 -0700

    Adds the AttributeAssignal module to include parsing the API response from Fulfil for a resource

---
+++ test/fulfil_api/resource/attribute_assignable_test.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module FulfilApi
+  class Resource
+    class AttributeAssignableTest < Minitest::Test
+      class Dummy
+        include FulfilApi::Resource::AttributeAssignable
+
+        attr_reader :attributes
+
+        def initialize(attributes = {})
+          @attributes = attributes
+        end
+      end
+
+      def setup
+        @resource = Dummy.new
+      end
+
+      def test_assigning_an_attribute
+        raw_value = "Main Warehouse"
+        @resource.assign_attribute("warehouse_name", raw_value)
+
+        assert_equal raw_value, @resource.attributes["warehouse_name"]
+      end
+
+      def test_assignign_an_nested_attribute
+        @resource.assign_attribute("warehouse.id", 10)
+
+        assert_equal 10, @resource.attributes["warehouse"]["id"]
+      end
+
+      def test_assigning_an_attribute_with_nested_hash
+        raw_value = { level_one: { level_two: { level_three: "level_four" } } }
+        @resource.assign_attribute(:hash, raw_value)
+
+        assert_equal(
+          { "level_one" => { "level_two" => { "level_three" => "level_four" } } },
+          @resource.attributes["hash"]
+        )
+      end
+
+      def test_assigning_attribute_names_for_nested_arrays
+        raw_value = { fields: [{ key: "value" }] }
+        @resource.assign_attribute(:array, raw_value)
+
+        assert_equal({ "fields" => [{ "key" => "value" }] }, @resource.attributes["array"])
+      end
+
+      def test_assigning_multiple_attributes_simultaneously
+        raw_values = { id: 1, warehouse_name: "Main Warehouse" }
+        @resource.assign_attributes(raw_values)
+
+        assert_equal raw_values[:warehouse_name], @resource.attributes["warehouse_name"]
+        assert_equal raw_values[:id], @resource.attributes["id"]
+      end
+
+      def test_extending_existing_attributes
+        raw_values = { warehouse: { id: 10 } }
+        @resource.assign_attribute("existing_attribute", raw_values)
+
+        assert_equal({ "warehouse" => { "id" => 10 } }, @resource.attributes["existing_attribute"])
+
+        @resource.assign_attribute("existing_attribute", { warehouse: { name: "Main Warehouse" } })
+
+        assert_equal(
+          { "warehouse" => { "id" => 10, "name" => "Main Warehouse" } },
+          @resource.attributes["existing_attribute"]
+        )
+      end
+
+      def test_overwriting_existing_attributes
+        raw_values = { warehouse: { id: 10 } }
+        @resource.assign_attribute("existing_attribute", raw_values)
+
+        assert_equal({ "warehouse" => { "id" => 10 } }, @resource.attributes["existing_attribute"])
+
+        @resource.assign_attribute("existing_attribute", { warehouse: { id: 15 } })
+
+        assert_equal({ "warehouse" => { "id" => 15 } }, @resource.attributes["existing_attribute"])
+      end
+    end
+  end
+end
