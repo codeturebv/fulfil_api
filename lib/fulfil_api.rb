be6db1a48e59dd80609932cdd472f2f0d8bb8773
Stefan Vermaas <stefan@codeture.nl>
2024-08-30 19:54:57 -0700

    Adds activesupport as an dependency to ease development

--- lib/fulfil_api.rb
+++ lib/fulfil_api.rb
@@ -5,6 +5,10 @@ require "zeitwerk"
 loader = Zeitwerk::Loader.for_gem
 loader.setup
 
+require "active_support"
+require "active_support/core_ext/hash/deep_merge"
+require "active_support/core_ext/hash/indifferent_access"
+
 module FulfilApi
 end
 
