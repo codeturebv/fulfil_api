924ca9a9cc43b10cbc6fa25df29af3f632a1e2e8
Stefan Vermaas <stefan@codeture.nl>
2024-09-02 10:45:52 -0400

    Adds support for enumeration, delegation and blank object method checks through activesupport

--- lib/fulfil_api.rb
+++ lib/fulfil_api.rb
@@ -6,8 +6,11 @@ loader = Zeitwerk::Loader.for_gem
 loader.setup
 
 require "active_support"
+require "active_support/core_ext/enumerable"
 require "active_support/core_ext/hash/deep_merge"
 require "active_support/core_ext/hash/indifferent_access"
+require "active_support/core_ext/module/delegation"
+require "active_support/core_ext/object/blank"
 
 module FulfilApi
 end
