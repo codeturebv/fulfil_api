dcb9320097c20394dc78a07df2ba9893cbfb79b8
Yannickdonners <yannickdonners@gmail.com>
2024-09-16 16:59:24 +0200

    Add test for CustomerShipmentResource

---
+++ test/fulfil_api/customer_shipment_test.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module FulfilApi
+  class CustomerShipmentTest < Minitest::Test
+    def setup
+      @customer_shipment = CustomerShipment.new(id: 123, model_name: CustomerShipment::MODEL_NAME)
+    end
+
+    def test_holding_shipment_successfully
+      stub_fulfil_request(:put, response: nil)
+
+      assert @customer_shipment.hold
+    end
+
+    def test_unholding_shipment_successfully
+      stub_fulfil_request(:put, response: nil)
+
+      assert @customer_shipment.unhold
+    end
+
+    def test_holding_fails_when_missing_id
+      refute CustomerShipment.new(model_name: CustomerShipment::MODEL_NAME).hold
+    end
+
+    def test_uholding_fails_when_missing_id
+      refute CustomerShipment.new(model_name: CustomerShipment::MODEL_NAME).unhold
+    end
+
+    def test_holding_fails_when_fulfil_returns_error
+      stub_fulfil_request(:put, response: { body: { message: "Missing Attributes" } }, status: 500)
+
+      refute @customer_shipment.hold
+    end
+
+    def test_unholding_fails_when_fulfil_returns_error
+      stub_fulfil_request(:put, response: { body: { message: "Missing Attributes" } }, status: 500)
+
+      refute @customer_shipment.unhold
+    end
+  end
+end
