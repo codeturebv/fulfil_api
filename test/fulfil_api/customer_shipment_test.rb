8e7db8604bc10d11dffeb4c7c88a5f845b5f4be8
Yannickdonners <yannickdonners@gmail.com>
2024-09-17 14:09:02 +0200

    Add more tests

--- test/fulfil_api/customer_shipment_test.rb
+++ test/fulfil_api/customer_shipment_test.rb
@@ -8,26 +8,76 @@ module FulfilApi
       @customer_shipment = CustomerShipment.new(id: 123, model_name: CustomerShipment::MODEL_NAME)
     end
 
-    def test_holding_shipment_successfully
+    def test_holding_shipment_successfully_from_instance
       stub_fulfil_request(:put, response: nil)
 
       assert @customer_shipment.hold
     end
 
-    def test_unholding_shipment_successfully
+    def test_holding_shipment_successfully_from_class
+      stub_fulfil_request(:put, response: nil)
+
+      assert CustomerShipment.hold!(123)
+    end
+
+    def test_holding_shipments_successfully_from_class
+      stub_fulfil_request(:put, response: nil)
+
+      assert CustomerShipment.hold!([123, 456])
+    end
+
+    def test_unholding_shipment_successfully_from_instance
       stub_fulfil_request(:put, response: nil)
 
       assert @customer_shipment.unhold
     end
 
-    def test_holding_makes_no_request_when_missing_id
-      assert_not_requested :put, /fulfil\.io/
-      CustomerShipment.new(model_name: CustomerShipment::MODEL_NAME).hold
+    def test_unholding_shipment_successfully_from_class
+      stub_fulfil_request(:put, response: nil)
+
+      assert CustomerShipment.unhold!(123)
     end
 
-    def test_uholding_makes_no_request_when_missing_id
-      assert_not_requested :put, /fulfil\.io/
-      CustomerShipment.new(model_name: CustomerShipment::MODEL_NAME).unhold
+    def test_unholding_shipments_successfully_from_class
+      stub_fulfil_request(:put, response: nil)
+
+      assert CustomerShipment.unhold!([123, 456])
+    end
+
+    def test_ensure_correct_body_for_holding_shipment
+      stub_fulfil_request(:put, response: nil)
+
+      assert CustomerShipment.hold!(123, note: "Double booking")
+
+      assert_requested :put, /fulfil\.io/ do |request|
+        assert_equal [[123], { "note" => "Double booking" }], JSON.parse(request.body)
+      end
+    end
+
+    def test_ensure_correct_body_for_unholding_shipment
+      stub_fulfil_request(:put, response: nil)
+
+      assert CustomerShipment.unhold!(123, note: "All good!")
+
+      assert_requested :put, /fulfil\.io/ do |request|
+        assert_equal [[123], { "note" => "All good!" }], JSON.parse(request.body)
+      end
+    end
+
+    def test_raises_error_when_holding_customer_shipment_fails_from_class
+      stub_fulfil_request(:put, response: { body: { message: "Missing Attributes" } }, status: 500)
+
+      assert_raises FulfilApi::Error do
+        CustomerShipment.hold!(123)
+      end
+    end
+
+    def test_raises_error_when_unholding_customer_shipment_fails_from_class
+      stub_fulfil_request(:put, response: { body: { message: "Missing Attributes" } }, status: 500)
+
+      assert_raises FulfilApi::Error do
+        CustomerShipment.unhold!(123)
+      end
     end
 
     def test_holding_fails_when_fulfil_returns_error
