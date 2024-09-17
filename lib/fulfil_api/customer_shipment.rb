186832cbca2d8e851153529e7922a1c9c1273439
Yannickdonners <yannickdonners@gmail.com>
2024-09-17 14:08:56 +0200

    Update comments from review

--- lib/fulfil_api/customer_shipment.rb
+++ lib/fulfil_api/customer_shipment.rb
@@ -9,40 +9,38 @@ module FulfilApi
     class << self
       # Sets the fulfillment status of the customer shipment on hold
       #
-      # @param id_or_ids [String, Integer, Array[String], Array[Integer]] The ID of the customer shipment to hold.
-      # @param note [String] A note to define th reason for holding. (Optional)
+      # @param id_or_ids [String, Integer, Array[String], Array[Integer]] The ID(s) of the customer shipment(s) to hold.
+      # @param note [String] A note to define the reason for holding. (Optional)
       # @param hold_reason [String] An hold reason ID. (Optional)
       # @return [Boolean] Returns true if hold successfully.
       # @raise [FulfilApi::Error] If an error occurs during holding the customer shipment.
       #
       # @example Hold a customer shipment
       #   FulfilApi::CustomerShipment.hold(123, note: "Double booking", hold_reason: hold_reason_id)
+      #
       # @example Hold multipe customer shipments
       #   FulfilApi::CustomerShipment.hold([123, 456], note: "Double booking", hold_reason: hold_reason_id)
       def hold!(id_or_ids, note: nil, hold_reason: nil)
-        if id_or_ids.present?
-          FulfilApi.client.put("/model/#{MODEL_NAME}/hold",
-                               body: [[id_or_ids], { note: note, hold_reason: hold_reason }.compact_blank])
-        end
+        FulfilApi.client.put("/model/#{MODEL_NAME}/hold",
+                             body: [[*id_or_ids].flatten, { note: note, hold_reason: hold_reason }.compact_blank])
 
         true
       end
 
       # Unholds the fulfillment status of the customer shipment
       #
-      # @param id_or_ids [String, Integer, Array[String], Array[Integer]] The ID of the customer shipment to unhold.
+      # @param id_or_ids [String, Integer, Array[String], Array[Integer]] The ID(s) of the customer shipment(s) to unhold.
       # @param note [String] A note to define the reason for unholding.
       # @return [Boolean] Returns true if hold successfully.
       # @raise [FulfilApi::Error] If an error occurs during holding the customer shipment.
       #
       # @example Unhold a customer shipment
       #   FulfilApi::CustomerShipment.unhold(123, note: "All clear")
+      #
       # @example Unhold a customer shipment
       #   FulfilApi::CustomerShipment.unhold([123, 456], note: "All clear")
       def unhold!(id_or_ids, note: nil)
-        if id_or_ids.present?
-          FulfilApi.client.put("/model/#{MODEL_NAME}/unhold", body: [[id_or_ids], { note: note }.compact_blank])
-        end
+        FulfilApi.client.put("/model/#{MODEL_NAME}/unhold", body: [[*id_or_ids].flatten, { note: note }.compact_blank])
 
         true
       end
@@ -50,7 +48,8 @@ module FulfilApi
 
     # Sets the current customer shipment on hold, rescuing any errors that occur and handling them based on error type.
     #
-    # @param attributes [Hash] The attributes to assign to the customer_shipment.
+    # @param note [String] A note to define the reason for holding. (Optional)
+    # @param hold_reason [String] An hold reason ID. (Optional)
     # @return [Boolean] Returns true if hold successfully, otherwise false.
     #
     # @example Holds a customer_shipment
