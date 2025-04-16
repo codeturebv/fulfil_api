d274ad7a961d9f0390bfc5745362021f3ba76593
Stefan Vermaas <stefan@codeture.nl>
2025-04-16 11:13:27 +0200

    Use a Thread.current instead of a Mutex

--- lib/fulfil_api/configuration.rb
+++ lib/fulfil_api/configuration.rb
@@ -16,8 +16,6 @@ module FulfilApi
     # @param options [Hash, nil] An optional list of configuration options.
     #   Each key in the hash should correspond to a configuration attribute.
     def initialize(options = {})
-      @mutex = Mutex.new
-
       # Assigns the optional configuration options
       options.each_pair do |key, value|
         send(:"#{key}=", value) if respond_to?(:"#{key}=")
@@ -25,25 +23,6 @@ module FulfilApi
 
       # Sets the default options if not provided
       set_default_options
-    end
-
-    # Provides thread-safe access to missing methods, allowing dynamic handling of configuration options.
-    #
-    # @param method [Symbol] The method name.
-    # @param args [Array] The arguments passed to the method.
-    # @param block [Proc] An optional block passed to the method.
-    # @return [void]
-    def method_missing(method, *args, &block)
-      @mutex.synchronize { super }
-    end
-
-    # Ensures that the object responds correctly to methods handled by method_missing.
-    #
-    # @param method [Symbol] The method name.
-    # @param include_private [Boolean] Whether to include private methods.
-    # @return [Boolean] Whether the object responds to the method.
-    def respond_to_missing?(method, include_private = false)
-      @mutex.synchronize { super }
     end
 
     private
@@ -61,13 +40,12 @@ module FulfilApi
   end
 
   @configuration = Configuration.new
-  @configuration_mutex = Mutex.new
 
   # Provides thread-safe access to the gem's configuration.
   #
   # @return [Fulfil::Configuration] The current configuration object.
   def self.configuration
-    @configuration_mutex.synchronize { @configuration }
+    Thread.current[:fulfil_api_configuration] ||= Configuration.new
   end
 
   # Allows the configuration of the gem in a thread-safe manner.
@@ -75,23 +53,25 @@ module FulfilApi
   # @yieldparam [Fulfil::Configuration] config The current configuration object.
   # @return [void]
   def self.configure
-    @configuration_mutex.synchronize { yield(@configuration) }
+    yield(configuration)
   end
 
   # Overwrites the configuration with the newly provided configuration options.
   #
   # @param options [Hash, Fulfil::Configuration] A list of configuration options for the gem.
   # @return [Fulfil::Configuration] The updated configuration object.
-  def self.configuration=(options_or_configuration)
-    @configuration_mutex.synchronize do
-      if options_or_configuration.is_a?(Hash)
-        options_or_configuration.each_pair do |key, value|
-          @configuration.send(:"#{key}=", value) if @configuration.respond_to?(:"#{key}=")
-        end
-      elsif options_or_configuration.is_a?(Configuration)
-        @configuration = options_or_configuration
+  def self.configuration=(options_or_configuration) # rubocop:disable Metrics/MethodLength
+    Thread.current[:fulfil_api_configuration] =
+      case options_or_configuration
+      when Hash
+        config = Configuration.new
+        options_or_configuration.each { |key, value| config.send(:"#{key}=", value) }
+        config
+      when Configuration
+        options_or_configuration
+      else
+        raise ArgumentError, "Expected Hash or Configuration, got #{options_or_configuration.class} instead"
       end
-    end
   end
 
   # Temporarily applies the provided configuration options within a block,
@@ -107,6 +87,6 @@ module FulfilApi
     yield
   ensure
     # Revert to the original configuration
-    self.configuration = original_configuration
+    Thread.current[:fulfil_api_configuration] = original_configuration
   end
 end
